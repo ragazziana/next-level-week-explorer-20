f804309bb0e3b953758204e08b73c9ea000b1304
Ana Ragazzi <greco_al@hotmail.com>
2022-07-06 10:51:27 +0100

    feat: transition when changing to dark/light mode

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function darkMode(){
     document.body.style.background = '#231f1f';
     document.body.style.color= '#ffffff';
+    document.body.style.transition='2s'
     document.getElementById('darkmode__moon').style.visibility='hidden';
     document.getElementById('darkmode__sun').style.visibility='visible';
 }
@@ -8,6 +9,7 @@ function darkMode(){
 function lightMode(){
     document.body.style.background = '#e6e6e6';
     document.body.style.color= '#000000';
+    document.body.style.transition='2s'
     document.getElementById('darkmode__sun').style.visibility='hidden';
     document.getElementById('darkmode__moon').style.visibility='visible';
 }
